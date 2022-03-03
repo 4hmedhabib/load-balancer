ffe01d01abe1c76c872c5274e33d6908b3b6bfe4
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-03-03 15:22:17 +0300

    added: Home screen and component

---
+++ components/HeaderTabs.jsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const HeaderTabs = () => {
+  return <View>{/* Header Button */}</View>;
+};
+
+export default HeaderTabs;
+
+const styles = StyleSheet.create({});

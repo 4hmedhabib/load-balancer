6b6dd3949a5b4fa4c951206969ef56c741b60ace
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-03-07 14:33:42 +0300

    updated: components

--- components/index.js
+++ components/index.js
@@ -1,2 +1,3 @@
 export { default as HeaderTabs } from "./HeaderTabs/HeaderTabs";
-export { default as SafeArea } from "./SafeArea";
+export { default as SearchBar } from "./SearchBar";
+export { default as Categories } from "./Categories";

98efa49df1d8a6ab93e4afe3740a71a50d5db8e7
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-03-07 13:23:51 +0300

    updated: some files and added some libriries

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,39 @@
-import { Text, StyleSheet } from "react-native";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  View,
+} from "react-native";
 import React from "react";
 import { HeaderTabs, SafeArea } from "../components";
 
 const Home = () => {
   return (
-    <SafeArea>
-      <HeaderTabs />
-    </SafeArea>
+    <SafeAreaView style={styles.screen}>
+      <View style={styles.headerTabContainer}>
+        <HeaderTabs />
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight + 10 : 0,
+    backgroundColor: "#eee",
+    flex: 1,
+  },
+  headerTabContainer: {
+    backgroundColor: "white",
+    display: "flex",
+    alignSelf: "center",
+    alignItems: "center",
+    width: "90%",
+    maxWidth: 300,
+    padding: 10,
+    borderRadius: 10,
+  },
+});

3d0146c8f05867f688f123c55f7b6637b15c4c96
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-03-07 14:39:24 +0300

    added: restuarant comonent

---
+++ components/Resturant/Item.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Item = () => {
+  return (
+    <View>
+      <Text>Item</Text>
+    </View>
+  );
+};
+
+export default Item;
+
+const styles = StyleSheet.create({});

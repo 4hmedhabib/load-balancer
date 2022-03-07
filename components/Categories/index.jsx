5fd059d3714eea6978f276d4b0d650241fea5370
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-03-07 16:23:12 +0300

    updated: categories

--- components/Categories/index.jsx
+++ components/Categories/index.jsx
@@ -2,38 +2,38 @@ import { StyleSheet, Image, View, Text, ScrollView } from "react-native";
 import React from "react";
 import Category from "./Category";
 
-const Categories = () => {
-  const items = [
-    {
-      image: require("../../assets/images/shopping-bag.png"),
-      text: "Pick-up",
-    },
-    {
-      image: require("../../assets/images/soft-drink.png"),
-      text: "Soft Drinks",
-    },
-    {
-      image: require("../../assets/images/bread.png"),
-      text: "Bakery Items",
-    },
-    {
-      image: require("../../assets/images/fast-food.png"),
-      text: "Fast Foods",
-    },
-    {
-      image: require("../../assets/images/deals.png"),
-      text: "Deals",
-    },
-    {
-      image: require("../../assets/images/coffee.png"),
-      text: "Coffee & Tea",
-    },
-    {
-      image: require("../../assets/images/desserts.png"),
-      text: "Desserts",
-    },
-  ];
+export const items = [
+  {
+    image: require("../../assets/images/shopping-bag.png"),
+    text: "Pick-up",
+  },
+  {
+    image: require("../../assets/images/soft-drink.png"),
+    text: "Soft Drinks",
+  },
+  {
+    image: require("../../assets/images/bread.png"),
+    text: "Bakery Items",
+  },
+  {
+    image: require("../../assets/images/fast-food.png"),
+    text: "Fast Foods",
+  },
+  {
+    image: require("../../assets/images/deals.png"),
+    text: "Deals",
+  },
+  {
+    image: require("../../assets/images/coffee.png"),
+    text: "Coffee & Tea",
+  },
+  {
+    image: require("../../assets/images/desserts.png"),
+    text: "Desserts",
+  },
+];
 
+const Categories = (props) => {
   return (
     <View style={styles.container}>
       <ScrollView horizontal showsHorizontalScrollIndicator={false}>

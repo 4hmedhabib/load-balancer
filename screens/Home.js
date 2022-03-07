924e4c3f298fffbe065e8806de87794816f9bc83
ahmedhabiib <info.ahmed.developer@gmail.com>
2022-03-07 16:45:13 +0300

    updated: some files

--- screens/Home.js
+++ screens/Home.js
@@ -16,8 +16,30 @@ import {
 
 import { items, localRestaurants } from "../data";
 
+const YELP_API_KEY =
+  "bdRJutLhFAQJ36t7b89CWjHFBU4OKzjt9wvZzcY-nkgmvTqlNMjZWV1eG7iBQ9R74SyfxRg9LWnBAkZY06BtAZAe4d2dfX-2vuX8a1l5V7foctHfX9UKEyoM5ts3YXYx";
+
 const Home = () => {
   const [restaurantItems, setRestaurantItems] = useState(localRestaurants);
+
+  const getRestaurantsFromYelp = () => {
+    const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
+
+    const apiOptions = {
+      headers: {
+        Authorization: `Bearer ${YELP_API_KEY}`,
+      },
+    };
+
+    return fetch(yelpUrl, apiOptions)
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseJson) => {
+        console.log("Restaurant", responseJson);
+        setRestaurantItems(responseJson.businesses);
+      });
+  };
 
   return (
     <SafeAreaView style={styles.screen}>
